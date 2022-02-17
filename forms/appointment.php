9810305e2fa376eb69c1dc3313016defbd1617bf
Adriano Hermida Maia <42121091+adrianohermida@users.noreply.github.com>
2022-02-17 14:24:46 -0300

    l

--- forms/appointment.php
+++ forms/appointment.php
@@ -21,7 +21,7 @@
   $contact->to = $receiving_email_address;
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
-  $contact->subject = 'Online Appointment Form';
+  $contact->subject = 'Confirmação de agendamento de consulta';
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   /*
